6c8f3def1b84d1154fc2dd2a9b84cb42152bda48
Денис Евдокимов <dsev.85@gmail.com>
2026-01-06 08:44:02 +0300

    Add project files.

---
+++ EVDOKIMOV.UI/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EVDOKIMOV.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Greeting"] = "Лабораторная работа №2";
+            return View();
+        }
+    }
+}
