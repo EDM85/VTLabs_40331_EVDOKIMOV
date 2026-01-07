2eb66ee084b9732db13484a114c813ec286ed87d
Денис Евдокимов <dsev.85@gmail.com>
2026-01-07 19:14:20 +0300

    LB_2_without class content

---
+++ EVDOKIMOV.UI/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace EVDOKIMOV.UI.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ViewData["controller"] = Request.RouteValues["controller"]
+                ?.ToString()
+                ?.ToLower()
+                ?? string.Empty;
+
+            ViewData["area"] = Request.RouteValues["area"]
+                ?.ToString()
+                ?.ToLower()
+                ?? string.Empty;
+
+            return View();
+        }   
+    }
+}
